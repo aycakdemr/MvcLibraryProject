710cfe99fa11a6f6c36a3af757ba41a6a44e8f31
Ayça Akdemir <mabeaaa@gmail.com>
2021-05-11 00:53:52 +0300

    11.05

---
+++ DataAccess/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class Context :DbContext
+    {
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookRecord> BookRecords { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<LateFee> LateFees { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Writer> Writer { get; set; }
+    }
+}
